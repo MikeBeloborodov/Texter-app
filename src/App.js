bda4b317d22cc124af21e99a55cfcc1ea69f437e
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-05-22 22:23:18 +0400

    finished connection to api

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import NavbarNew from "./components/NavbarNew";
 import LoginForm from "./components/LoginForm";
 
 export default function App(){
+
+    const [isLogged, setIsLogged] = React.useState(false)
+    const [userToken, setUserToken] = React.useState({})
+
     return(
         <div>
             <div>
                 <NavbarNew />
             </div>
             <div style={{width: "400px", margin: "0 auto", padding: "100px 0 0 0"}}>
-                <LoginForm />
+                {!isLogged && <LoginForm 
+                                loginHandle={setIsLogged}
+                                tokenHandle={setUserToken}
+                                />}
+                {isLogged && <h1>You are now logged in!</h1>}
             </div>
         </div>
     )
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,39 +1,80 @@
 import React from "react"
 import {Form, Button, Card} from "react-bootstrap"
+import { sprintf } from "sprintf-js"
 
 
-export default function LoginForm(){
+export default function LoginForm({loginHandle, tokenHandle}){
+
+    URL = process.env.REACT_APP_API_LOGIN_URL
     
-    const [userPasswordInput, setuserPasswordInput] = React.useState("")
-    const [userInputEmail, setUserInputEmail] = React.useState("")
+    const [formData, setFormData] = React.useState({
+        "email": "",
+        "password": "",
+        "submit": 0
+    })
     
-    function handleMouseClick(){
-        console.log("Email: " + userInputEmail)
-        console.log("Password: " + userPasswordInput)
-    }
+    React.useEffect(() => {
+        if (formData.submit){
+            fetch(URL, {method: "POST",
+                        body: sprintf('username=%s&password=%s', formData.email, formData.password),
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                                }
+                    }).then( (res) => {
+                        if (res.status === 200){
+                            loginHandle(true)
+                        }else{
+                            alert("Wrong credentials. Try again.")
+                        }
+                    })
+        }
+    }, [formData.submit])       
 
-    function handleEmailInput(event){
-        setUserInputEmail(oldValue => event.target.value)
+    function handleFormData(event){
+        const {name, value} = event.target
+        
+        setFormData(oldValues => {
+            return({
+                ...oldValues,
+                [name]: value
+            })
+        })
     }
-
-    function handlePasswordInput(event){
-        setuserPasswordInput(oldValue => event.target.value)
+    
+    function handleSubmit(event){
+        event.preventDefault()
+        setFormData(oldValues => {
+            return ({
+                ...oldValues,
+                "submit": oldValues.submit + 1
+            })
+        })
     }
 
     return(
         <Card className="mb-3" bg="primary" text="light">
         <div style={{padding: "10px 10px 10px"}}>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onChange={handleEmailInput}/>
+                    <Form.Control
+                     type="email" 
+                     name="email"
+                     placeholder="Enter email" 
+                     onChange={handleFormData}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={handlePasswordInput}/>
+                    <Form.Control 
+                    type="password" 
+                    name="password"
+                    placeholder="Password" 
+                    onChange={handleFormData}
+                />
                 </Form.Group>
-                <Button variant="light" type="submit" onClick={handleMouseClick}>
+                <Button variant="light" type="submit">
                     Submit
                 </Button>
             </Form>

5419337f2b842e81b7e5c18eccf808ae30f99487
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-05-24 16:34:14 +0400

    added delete functionality

--- src/components/PostActionsDropdown.js
+++ src/components/PostActionsDropdown.js
@@ -4,8 +4,12 @@ import AlertModal from "./AlertModal"
 
 export default function PostActionDropdown({id, userToken, url_list, setPostChangedState}){
     // get id for a liked post and set it, default id is 0
-    const [like, setLike] = React.useState(0)
-    // alert modal for various purposes
+    const [userAction, setUserAction] = React.useState({
+        like: 0,
+        update: 0,
+        delete: 0
+    })
+
     const [showAlertModal, setAlertModal] = React.useState({
         "show": false,
         "title": "",
@@ -13,8 +17,8 @@ export default function PostActionDropdown({id, userToken, url_list, setPostChan
     })
     // effect to send a like to the server
     React.useEffect(() => {
-        if(like !== 0) {
-            fetch(url_list.POSTS_LIKE_URL + like, {method: "PATCH",
+        if(userAction.like !== 0) {
+            fetch(url_list.POSTS_LIKE_URL + userAction.like, {method: "PATCH",
                         headers:{
                             "Content-Type": "application/json",
                             "Authorization": `Bearer ${userToken.access_token}`
@@ -47,17 +51,101 @@ export default function PostActionDropdown({id, userToken, url_list, setPostChan
                 }
             })
             .then(data => {
-                setLike(0)
+                setUserAction(() =>{
+                    return ({
+                        like: 0,
+                        update: 0,
+                        delete: 0
+                    })
+                })
                 if (data === "problem"){
                     return
                 }
                 setPostChangedState(true)
             })
         }
-    },[like])
+    },[userAction.like])
 
+    // effect to delete a post
+    React.useEffect(() => {
+        if(userAction.delete !== 0) {
+            fetch(url_list.POSTS_URL + userAction.delete, {method: "DELETE",
+                        headers:{
+                            "Content-Type": "application/json",
+                            "Authorization": `Bearer ${userToken.access_token}`
+                        }
+                    })
+            .then(res => {
+                if (res.status === 200){
+                    return res.json()
+                }else if (res.status === 403){
+                    setAlertModal({
+                            "show": true,
+                            "title": "Access problem",
+                            "body": "You can only delete your own posts."
+                                })
+                    return "problem"
+                }else if (res.status === 404){
+                    setAlertModal({
+                        "show": true,
+                        "title": "Post does not exist",
+                        "body": "The post you are trying to delete does not exist."
+                            })
+                    return "problem"
+                }else{
+                    setAlertModal({
+                            "show": true,
+                            "title": "Server problem",
+                            "body": "Something is wrong on the server side, please contact administrator."
+                                })
+                    return "problem"
+                }
+            })
+            .then(data => {
+                setUserAction(() =>{
+                    return ({
+                        like: 0,
+                        update: 0,
+                        delete: 0
+                    })
+                })
+                if (data === "problem"){
+                    return
+                }
+                setPostChangedState(true)
+            })
+        }
+    
+    }, [userAction.delete])
+    
     function handleLike(){
-        setLike(id)
+        setUserAction(() =>{
+            return ({
+                like: id,
+                update: 0,
+                delete: 0
+            })
+        })
+    }
+
+    function handleUpdate(){
+        setUserAction(() =>{
+            return ({
+                like: 0,
+                update: id,
+                delete: 0
+            })
+        })
+    }
+
+    function handleDelete(){
+        setUserAction(() =>{
+            return ({
+                like: 0,
+                update: 0,
+                delete: id
+            })
+        })
     }
 
     return(
@@ -73,8 +161,8 @@ export default function PostActionDropdown({id, userToken, url_list, setPostChan
 
                 <Dropdown.Menu>
                     <Dropdown.Item onClick={handleLike}>Like</Dropdown.Item>
-                    <Dropdown.Item>Update</Dropdown.Item>
-                    <Dropdown.Item>Delete</Dropdown.Item>
+                    <Dropdown.Item onClick={handleUpdate}>Update</Dropdown.Item>
+                    <Dropdown.Item onClick={handleDelete}>Delete</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
         </div>
